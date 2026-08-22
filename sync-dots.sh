ec94eff06f786d3289a58da8e522aa7ddb9544b0
brokendraft <user@desktop-aix.lan>
2026-08-22 14:12:04 +0200

    add distrobox init

--- sync-dots.sh
+++ sync-dots.sh
@@ -2,7 +2,10 @@
 
 mkdir -p .config/sway
 mkdir -p .config/waybar
+mkdir -p .config/distrobox
 
+
+cp -rf ~/.config/distrobox/* .config/distrobox
 cp -rf ~/.mydotrc .mydotrc
 cp -rf ~/.config/sway/* .config/sway
 cp -rf ~/.config/waybar/* .config/waybar
