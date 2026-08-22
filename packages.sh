a520fa2f7c2b808f61cece964e0fe55a93090103
brokendraft <brokendraft@pm.me>
2026-08-22 17:09:23 +0200

    rename steam.deb

--- packages.sh
+++ packages.sh
@@ -2,6 +2,6 @@
 
 cd ~/Downloads/
 
-curl -J -L "https://cdn.fastly.steamstatic.com/client/installer/steam.deb" --output discord.deb
+curl -J -L "https://cdn.fastly.steamstatic.com/client/installer/steam.deb" --output steam.deb
 curl -J -L "https://discord.com/api/download?platform=linux&format=deb" --output discord.deb
 
